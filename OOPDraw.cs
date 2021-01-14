cd9128822fb4d37dd69112ce4a87ad50e1d69851
Thomas Stanger <thomasstanger0@gmail.com>
2021-01-14 15:41:03 +0000

    Add project files.

---
+++ OOPDraw.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing
+{
+    public partial class OOPDraw : Form
+    {
+        public OOPDraw()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        Pen currentPen = new Pen(Color.Red);
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            currentPen.Width = 5;
+            var gr = e.Graphics;
+            var a = new Point(20,30);
+            var b = new Point(400,500);
+            gr.DrawLine(currentPen,a,b);
+            gr.
+        }
+    }
+}
