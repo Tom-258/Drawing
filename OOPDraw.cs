49c77f0161fc434e4ce00b9bd93251c84419e921
Thomas Stanger <thomasstanger0@gmail.com>
2021-01-15 12:02:38 +0000

    exercise 4

--- OOPDraw.cs
+++ OOPDraw.cs
@@ -23,23 +23,30 @@ namespace Drawing
         private bool dragging = false;
         Point startOfDrag = Point.Empty;
         Point lastMousePosition = Point.Empty;
+        List<Line> lines = new List<Line>();
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
             var gr = e.Graphics;
-            gr.DrawLine(currentPen,startOfDrag,lastMousePosition);
+            foreach (var line in lines)
+            {
+                line.Draw(gr);
+            }
         }
 
         private void OOPDraw_MouseDown(object sender, MouseEventArgs e)
         {
             dragging = true;
             startOfDrag = lastMousePosition = e.Location;
+            lines.Add(new Line(currentPen, e.X, e.Y));
         }
 
         private void OOPDraw_MouseMove(object sender, MouseEventArgs e)
         {
             if (dragging)
             {
+                var currentLine = lines.Last();
+                currentLine.GrowTo(e.X,e.Y);
                 lastMousePosition = e.Location;
                 Refresh();
             }
