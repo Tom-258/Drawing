cf2b14191e1360ed3ff7f398372aacaedeebbfdb
Thomas Stanger <thomasstanger0@gmail.com>
2021-01-15 12:21:38 +0000

    exercise 5

--- OOPDraw.Designer.cs
+++ OOPDraw.Designer.cs
@@ -29,24 +29,80 @@
         private void InitializeComponent()
         {
             this.Canvas = new System.Windows.Forms.PictureBox();
+            this.LineWidth = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Colour = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // Canvas
             // 
             this.Canvas.BackColor = System.Drawing.SystemColors.Window;
-            this.Canvas.Location = new System.Drawing.Point(204, 12);
+            this.Canvas.Location = new System.Drawing.Point(198, 42);
             this.Canvas.Name = "Canvas";
-            this.Canvas.Size = new System.Drawing.Size(1177, 597);
+            this.Canvas.Size = new System.Drawing.Size(957, 478);
             this.Canvas.TabIndex = 0;
             this.Canvas.TabStop = false;
+            this.Canvas.Click += new System.EventHandler(this.Canvas_Click);
             this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            // 
+            // LineWidth
+            // 
+            this.LineWidth.FormattingEnabled = true;
+            this.LineWidth.Items.AddRange(new object[] {
+            "Thin",
+            "Medium",
+            "Thick"});
+            this.LineWidth.Location = new System.Drawing.Point(25, 60);
+            this.LineWidth.Name = "LineWidth";
+            this.LineWidth.Size = new System.Drawing.Size(121, 23);
+            this.LineWidth.TabIndex = 1;
+            this.LineWidth.SelectedIndexChanged += new System.EventHandler(this.LineWidth_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Line Width";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Colour
+            // 
+            this.Colour.FormattingEnabled = true;
+            this.Colour.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue",
+            "Black"});
+            this.Colour.Location = new System.Drawing.Point(25, 144);
+            this.Colour.Name = "Colour";
+            this.Colour.Size = new System.Drawing.Size(121, 23);
+            this.Colour.TabIndex = 3;
+            this.Colour.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pen Color";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // OOPDraw
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1404, 621);
+            this.ClientSize = new System.Drawing.Size(1236, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Colour);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LineWidth);
             this.Controls.Add(this.Canvas);
             this.Name = "OOPDraw";
             this.Text = "OOPDraw";
@@ -55,12 +111,17 @@
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OOPDraw_MouseUp);
             ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.ComboBox LineWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Colour;
+        private System.Windows.Forms.Label label2;
     }
 }
 
